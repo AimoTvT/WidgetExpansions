e444edb4bea3c5a7c6e9969785b78cb5cd427298
Aimo <2422489737@qq.com>
2024-05-16 23:36:40 +0800

    更新版本

--- Source/WidgetExpansions/Public/SuspendedWidget/SuspendedWidget.h
+++ Source/WidgetExpansions/Public/SuspendedWidget/SuspendedWidget.h
@@ -1,6 +1,5 @@
 /**
  * Copyright: Aimo_皑墨
- * Open source protocol: MIT License
  * Open Source Date: Jun 27, 2023
  * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
  * Open source address(Git): https://github.com/AimoTvT/WidgetExpansions
@@ -8,22 +7,20 @@
  * Build powerful plugins together!!
  *
  * 版权所有权: Aimo_皑墨
- * 开源协议: MIT License
  * 开源时间: 2023年6月27日
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
  * 开源地址(Git): https://github.com/AimoTvT/WidgetExpansions
  * 欢迎有实力的大佬/萌新加入本插件的贡献
  * 一起打造强大的插件!!!
  */
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "UMG/Public/Components/Image.h"
-#include "UMG/Public/Components/SizeBox.h"
-#include "UMG/Public/Components/TextBlock.h"
-#include "UMG/Public/Components/HorizontalBox.h"
+#include "Components/Image.h"
+#include "Components/SizeBox.h"
+#include "Components/TextBlock.h"
+#include "Components/HorizontalBox.h"
 
 #include "SuspendedWidget.generated.h"
 
--- Source/WidgetExpansions/WidgetExpansions.Build.cs
+++ Source/WidgetExpansions/WidgetExpansions.Build.cs
@@ -1,6 +1,5 @@
 /**
  * Copyright: Aimo_皑墨
- * Open source protocol: MIT License
  * Open Source Date: Jun 27, 2023
  * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
  * Open source address(Git): https://github.com/AimoTvT/WidgetExpansions
@@ -8,7 +7,6 @@
  * Build powerful plugins together!!
  *
  * 版权所有权: Aimo_皑墨
- * 开源协议: MIT License
  * 开源时间: 2023年6月27日
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
  * 开源地址(Git): https://github.com/AimoTvT/WidgetExpansions
@@ -56,7 +54,6 @@ public class WidgetExpansions : ModuleRules
 				"SlateCore",
 				"UMG",
                 "ResourcesExpansions",
-                "UniversalInterface",
                 "UniversalFunctionLibrary",
 				// ... add private dependencies that you statically link with here ...	
 			}
